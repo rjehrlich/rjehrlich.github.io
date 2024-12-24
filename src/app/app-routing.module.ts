b341463e574bd76076675cbced609a5ec21d633a
rjsehrlich <racheljsehrlich@gmail.com>
2024-12-24 14:20:00 -0700

    add Material UI and test toggle slider

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 const routes: Routes = [];
 
