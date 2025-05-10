978522b82883677d3a449c24a017fef4d00bbcf7
rjsehrlich <racheljsehrlich@gmail.com>
2025-05-10 13:06:06 -0700

    set up header & footer  componenet

--- src/app/shared/header-nav/header-nav.component.ts
+++ src/app/shared/header-nav/header-nav.component.ts
@@ -1,11 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
-    selector: 'app-header-nav',
-    templateUrl: './header-nav.component.html',
-    styleUrls: ['./header-nav.component.css'],
-    standalone: false
+  selector: 'app-header',
+  templateUrl: './header-nav.component.html',
+  styleUrls: ['./header-nav.component.css']
 })
-export class HeaderNavComponent {
+export class HeaderComponent {
+  menuVisible = false;
+  locked = false;
 
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const banner = document.getElementById('banner');
+    const header = document.getElementById('header');
+    if (!banner || !header) return;
+
+    const headerHeight = header.offsetHeight;
+    const bannerBottom = banner.getBoundingClientRect().bottom;
+
+    if (bannerBottom < headerHeight) {
+      header.classList.remove('alt');
+    } else {
+      header.classList.add('alt');
+    }
+  }
+
+  toggleMenu(): void {
+    if (this.locked) return;
+    this.locked = true;
+
+    this.menuVisible = !this.menuVisible;
+    setTimeout(() => {
+      this.locked = false;
+    }, 350);
+  }
+
+  hideMenu(): void {
+    if (this.locked) return;
+    this.locked = true;
+
+    this.menuVisible = false;
+    setTimeout(() => {
+      this.locked = false;
+    }, 350);
+  }
 }
+
